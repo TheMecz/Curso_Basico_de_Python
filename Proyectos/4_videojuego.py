2bf65d9ecab0710d0f010b38d6d1af31715a37f2
TheMecz <maxponcesan@gmail.com>
2021-10-30 14:54:41 -0500

    repasando python

--- Proyectos/4_videojuego.py
+++ Proyectos/4_videojuego.py
@@ -4,20 +4,20 @@ def run():
     num_random = random.randint(1, 100)
     num_elegido = int(input('Elige un número del 1 al 100: '))
     vidas = 5
-    if vidas > 0: 
+    if vidas > 0:
         while num_random != num_elegido:
             if vidas == 0:
                 break
             elif num_elegido < num_random:
                 print('Piensa en un número más grande')
             else:
-                print('Piensa en un número más pequiño')
+                print('Piensa en un número más pequeño')
             vidas -= 1
             print('Te quedan ' + str(vidas) + ' vidas ♥')
             num_elegido = int(input('Elige un número del 1 al 100: '))
         print('¡FELICIDADES! tú número coincide con el número seleccionado por la computadora 😃')
     else:
         print("¡Game Over!")
-            
+
 if __name__ == '__main__':
     run()
